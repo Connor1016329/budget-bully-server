0f529b164a4ae1d43187b05d06ef9b4ce3228a19
Connor1016329 <connorneistadt@live.com>
2024-10-14 19:41:29 -0600

    hotfix

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { ClerkController } from './webhook.controller';
 import { ClerkService } from './clerk.service';
 import { DatabaseModule } from '../database/database.module';
+import { PlaidService } from '../plaid/plaid.service';
+import { PlaidModule } from '../plaid/plaid.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, PlaidModule],
   controllers: [ClerkController],
-  providers: [AuthService, ClerkService],
+  providers: [AuthService, ClerkService, PlaidService],
 })
 export class AuthModule {}
